e594ec39e376ad7a499133b81af04577f8833906
Shailesh <shailesh.parmar@deuexsolutions.com>
2021-03-22 14:41:29 +0530

    implemented anime search from openAPI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import AnimeList from "./component/AnimeList";
+
+const apiRequest = async (search) => {
+  const api = `https://api.jikan.moe/v3/search/anime?q=${search}`;
+  return await fetch(api).then((res) => res.json());
+};
 
 function App() {
+  const [animeSearch, setanimeSearch] = useState("");
+  const [apiData, setApiData] = useState({});
+  const [gotData, setgotData] = useState(false);
+
+  const handleSubmite = async (event) => {
+    event.preventDefault();
+    const data = await apiRequest(animeSearch).then((res) => res);
+    setApiData(data);
+    setgotData(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Anime Search</h1>
+      <form onSubmit={handleSubmite} className="search">
+        <input
+          type="text"
+          value={animeSearch}
+          onChange={(e) => setanimeSearch(e.target.value)}
+          placeholder="search for anime, e.g Pokemon"
+          required
+        />
+        <button>Go</button>
+      </form>
+      {gotData ? (
+        <AnimeList apiData={apiData} />
+      ) : (
+        <h2>Search for Anime to see results</h2>
+      )}
     </div>
   );
 }
